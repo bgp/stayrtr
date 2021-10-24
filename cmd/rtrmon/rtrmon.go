876179b0d9a9a47d44077fb6720c6f3a40be59f8
Job Snijders <job@sobornost.net>
2021-10-24 14:24:08 +0000

    Use specific prometheus exporter port for rtrmon

--- cmd/rtrmon/rtrmon.go
+++ cmd/rtrmon/rtrmon.go
@@ -43,7 +43,7 @@ var (
 	AppVersion = "RTRmon " + version + " " + buildinfos
 
 	OneOff      = flag.Bool("oneoff", false, "dump as json and exits")
-	Addr        = flag.String("addr", ":8080", "Server address")
+	Addr        = flag.String("addr", ":9866", "Server address")
 	MetricsPath = flag.String("metrics", "/metrics", "Metrics path")
 	OutFile     = flag.String("file", "diff.json", "Diff file (or URL path without /)")
 
